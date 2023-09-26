36d3427f03d5bfe4f5cac569a48a9362c4390ad6
fathurrsyah <144696955+fathurrsyah@users.noreply.github.com>
2023-09-26 19:44:50 +0700

    FathurRahmanSyahputra
    MidTest_Dibimbing_SQL2

---
+++ MID_TEST/Input_Test_SQL_2_PostgreSQL.sql
@@ -0,0 +1,14 @@
+CREATE TABLE email_list (
+  ID INT PRIMARY KEY,
+  email VARCHAR(255)
+);
+
+INSERT INTO email_list (ID, email)
+VALUES
+  (1, 'ayam@gmail.com'),
+  (2, 'sapi@gmail.com'),
+  (3, 'ayam@gmail.com'),
+  (4, 'ayam@gmail.com'),
+  (5, 'sapi@gmail.com'),
+  (6, 'kuda@gmail.com'),
+  (7, 'kambing@gmail.com');
