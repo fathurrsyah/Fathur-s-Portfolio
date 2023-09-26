56041c0750d103f6779ab1ae677a7657a5d76626
fathurrsyah <144696955+fathurrsyah@users.noreply.github.com>
2023-09-26 19:34:55 +0700

    Fathur Rahman Syahputra
    Input data

---
+++ MID_TEST/Input_Test_SQL_1_PostgreSQL.sql
@@ -0,0 +1,16 @@
+CREATE TABLE input_table (
+  id INT PRIMARY KEY,
+  num INT
+);
+
+INSERT INTO input_table (id, num)
+VALUES
+  (1, 1),
+  (2, 2),
+  (3, 2),
+  (4, 3),
+  (5, 3),
+  (6, 3),
+  (7, 4),
+  (8, 4),
+  (9, 4);
